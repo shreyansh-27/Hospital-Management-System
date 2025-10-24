e04ca42f542f949371cc53e9c00ec33e2ed20e46
Shreyansh Bhardwaj <shreyansh272002@gmail.com>
2025-10-25 00:59:08 +0530

    Patient service and imple added

--- backend/src/main/java/com/meow/hospitalmanagementsystem/service/PatientService.java
+++ backend/src/main/java/com/meow/hospitalmanagementsystem/service/PatientService.java
@@ -3,20 +3,18 @@ package com.meow.hospitalmanagementsystem.service;
 import com.meow.hospitalmanagementsystem.dto.PatientRequestDTO;
 import com.meow.hospitalmanagementsystem.dto.PatientResponseDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface PatientService {
 
     ResponseEntity<List<PatientResponseDTO>> getAllPatients();
 
     ResponseEntity<PatientResponseDTO> getPatientById(Long patientId);
 
-    ResponseEntity<PatientRequestDTO> addPatient(PatientRequestDTO patientDTO);
+    ResponseEntity<PatientResponseDTO> addPatient(PatientRequestDTO patientDTO);
 
-    ResponseEntity<PatientRequestDTO> updatePatient(PatientRequestDTO patientRequestDTO);
+    ResponseEntity<PatientResponseDTO> updatePatient(PatientRequestDTO patientRequestDTO, Long id);
 
     ResponseEntity<String> deletePatient(Long patientId);
 }
--- backend/src/main/java/com/meow/hospitalmanagementsystem/service/PatientServiceImpl.java
+++ backend/src/main/java/com/meow/hospitalmanagementsystem/service/PatientServiceImpl.java
@@ -2,33 +2,94 @@ package com.meow.hospitalmanagementsystem.service;
 
 import com.meow.hospitalmanagementsystem.dto.PatientRequestDTO;
 import com.meow.hospitalmanagementsystem.dto.PatientResponseDTO;
+import com.meow.hospitalmanagementsystem.exception.InvalidRequestException;
+import com.meow.hospitalmanagementsystem.exception.ResourceNotFoundException;
+import com.meow.hospitalmanagementsystem.mapper.PatientMapper;
+import com.meow.hospitalmanagementsystem.model.Patient;
+import com.meow.hospitalmanagementsystem.model.enums.Status;
+import com.meow.hospitalmanagementsystem.repo.PatientRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PatientServiceImpl implements PatientService{
+
+    private final PatientRepo patientRepo;
+    
+    PatientServiceImpl(PatientRepo patientRepo){
+        this.patientRepo = patientRepo;
+    }
+
     @Override
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
-        return null;
+
+        List<PatientResponseDTO> patientResponseDTOS = patientRepo.findByStatus(Status.ACTIVE)
+                .stream()
+                .map(PatientMapper::toPatientResponse)
+                .toList();
+
+        if(patientResponseDTOS.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(patientResponseDTOS);
     }
 
     @Override
     public ResponseEntity<PatientResponseDTO> getPatientById(Long patientId) {
-        return null;
+        Patient patient = patientRepo.findById(patientId)
+                .orElseThrow(() -> new ResourceNotFoundException("Patient", patientId));
+
+       return new ResponseEntity<>(PatientMapper.toPatientResponse(patient), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<PatientRequestDTO> addPatient(PatientRequestDTO patientDTO) {
-        return null;
+    public ResponseEntity<PatientResponseDTO> addPatient(PatientRequestDTO patientDTO) {
+        Patient patient = PatientMapper.toPatient(patientDTO);
+
+        boolean exists =
+                patientRepo.findPatientByPhoneNumber(patient.getPhoneNumber()).isPresent() ||
+                patientRepo.findPatientByEmail(patient.getEmail()).isPresent();
+
+        if(exists){
+            throw new InvalidRequestException("Patient already exists");
+        }
+        System.out.println(patient);
+        patient.setStatus(Status.ACTIVE);
+
+        Patient newPatient = patientRepo.save(patient);
+        return new ResponseEntity<>(PatientMapper.toPatientResponse(newPatient), HttpStatus.CREATED);
+
     }
 
     @Override
-    public ResponseEntity<PatientRequestDTO> updatePatient(PatientRequestDTO patientRequestDTO) {
-        return null;
+    public ResponseEntity<PatientResponseDTO> updatePatient(PatientRequestDTO patientRequestDTO, Long id) {
+
+        Patient patient = (Patient) patientRepo.findByIdAndStatus(id, Status.ACTIVE).
+                orElseThrow(() -> new ResourceNotFoundException("Patient", id));
+
+        PatientMapper.updatedPatient(patient, patientRequestDTO);
+
+        Patient updatedPatient = patientRepo.save(patient);
+        return ResponseEntity.ok(PatientMapper.toPatientResponse(updatedPatient));
     }
 
     @Override
     public ResponseEntity<String> deletePatient(Long patientId) {
-        return null;
+            Patient patient = patientRepo
+                            .findById(patientId)
+                            .orElseThrow(() -> new ResourceNotFoundException("Patient", patientId));
+            if(patient.getStatus() == Status.INACTIVE){
+                throw new InvalidRequestException("Patient is already inactive");
+            }
+
+            patient.setStatus(Status.INACTIVE);
+            patientRepo.save(patient);
+            return ResponseEntity.ok("Patient Deleted Successfully");
     }
+
 }
